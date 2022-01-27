5985b6b8fe23486abe6a4e128166f57e415e9c2d
pboucher44 <boucher.pierre44400@hotmail.fr>
2022-01-27 20:35:07 +0100

    Starting beautiful menu

--- script.js
+++ script.js
@@ -4,17 +4,20 @@ const MENU_RESUME = document.getElementById('menu-resume');
 const MENU_SERVICES = document.getElementById('menu-services');
 const MENU_CONTACT = document.getElementById('menu-contact');
 
+calculActiveMenu();
+
 window.addEventListener('scroll', (elem) => {
-    let pourcentageScroll = window.scrollY * 100 / window.innerHeight;
-    calculActiveMenu(pourcentageScroll);    
+    calculActiveMenu();    
 })
 
-function calculActiveMenu(pourcentageScroll) {
+function calculActiveMenu() {
     MENU_HOME.classList.remove('active');
     MENU_ABOUT.classList.remove('active');
     MENU_RESUME.classList.remove('active');
     MENU_SERVICES.classList.remove('active');
     MENU_CONTACT.classList.remove('active');
+
+    const pourcentageScroll = window.scrollY * 100 / window.innerHeight;
 
     if (pourcentageScroll < 100) {
         return MENU_HOME.classList.add('active');
